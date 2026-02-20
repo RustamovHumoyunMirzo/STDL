078fcbbac24858deda180a9f8b68ce1f4c35c014
HumoyunbekDev <149808645+HumoyunbekDev@users.noreply.github.com>
2026-02-20 20:03:48 +0500

    Circular reference detector

--- src/parser.cpp
+++ src/parser.cpp
@@ -2,15 +2,41 @@
 #include <tao/pegtl.hpp>
 #include <iostream>
 #include <sstream>
+#include <unordered_map>
+#include <unordered_set>
 
 namespace STDLParser {
 namespace pegtl = TAO_PEGTL_NAMESPACE;
 
+static bool hasPath(
+    Node* from,
+    Node* to,
+    const std::unordered_map<Node*, std::unordered_set<Node*>>& graph,
+    std::unordered_set<Node*>& visited)
+{
+    if(from == to) return true;
+    if(!graph.count(from)) return false;
+
+    for(Node* next : graph.at(from)){
+        if(visited.insert(next).second){
+            if(hasPath(next, to, graph, visited))
+                return true;
+        }
+    }
+    return false;
+}
+
 struct ParserState {
     Scene* scene = nullptr;
     std::vector<NodePtr> nodeStack;
+
     bool inList = false;
     std::vector<std::shared_ptr<ValueNode>> currentList;
+
+    std::unordered_map<Node*, std::unordered_set<Node*>> refGraph;
+
+    std::unordered_map<int, Node*> localIDMap;
+    std::unordered_map<int, Node*> globalIDMap;
 };
 
 inline std::string unquote(const std::string& str){
@@ -105,29 +131,45 @@ template<> struct Action<grammar::local_ref>{
     template<typename Input>
     static void apply(const Input& in, ParserState& state){
         if(state.nodeStack.empty()) return;
-        
+
+        Node* from = state.nodeStack.back().get();
+
         std::string text = in.string();
         size_t hash = text.find('#');
         size_t gt = text.find('>');
-        
+
         Ref ref;
         if(hash != std::string::npos && gt != std::string::npos){
-            std::string idStr = text.substr(hash + 1, gt - hash - 1);
-            ref.localID = std::stoi(idStr);
+            ref.localID = std::stoi(text.substr(hash + 1, gt - hash - 1));
+        }
+
+        if (ref.localID.has_value()) {
+            int id = ref.localID.value();
+
+            auto it = state.localIDMap.find(id);
+            if(it != state.localIDMap.end()){
+                Node* to = it->second;
             
-            if(hash > 1){
-                std::string typeStr = text.substr(1, hash - 1);
-                if(!typeStr.empty()) ref.type = typeStr;
+                std::unordered_set<Node*> visited;
+                if(hasPath(to, from, state.refGraph, visited)){
+                    throw pegtl::parse_error(
+                        "Circular reference detected (local)",
+                        in
+                    );
+                }
+            
+                state.refGraph[from].insert(to);
             }
         }
-        
+
         auto valNode = std::make_shared<ValueNode>();
         valNode->value = ref;
-        
+
         if(state.inList){
             state.currentList.push_back(valNode);
         } else {
-            state.nodeStack.back()->properties["_last_value"] = std::vector<std::shared_ptr<ValueNode>>{valNode};
+            state.nodeStack.back()->properties["_last_value"] =
+                std::vector<std::shared_ptr<ValueNode>>{valNode};
         }
     }
 };
@@ -136,38 +178,60 @@ template<> struct Action<grammar::global_ref>{
     template<typename Input>
     static void apply(const Input& in, ParserState& state){
         if(state.nodeStack.empty()) return;
-        
+
+        Node* from = state.nodeStack.back().get();
+
         std::string text = in.string();
         size_t colon = text.find(':');
         size_t at = text.find('@');
         size_t gt = text.find('>');
-        
+
         Ref ref;
         if(at != std::string::npos && gt != std::string::npos){
-            std::string idStr = text.substr(at + 1, gt - at - 1);
-            ref.globalID = std::stoi(idStr);
-            
+            ref.globalID = std::stoi(text.substr(at + 1, gt - at - 1));
+
             if(colon != std::string::npos){
                 if(colon > 1){
-                    std::string typeStr = text.substr(1, colon - 1);
-                    if(!typeStr.empty()) ref.type = typeStr;
+                    ref.type = text.substr(1, colon - 1);
                 }
                 if(at > colon + 1){
-                    std::string nameStr = text.substr(colon + 1, at - colon - 1);
-                    nameStr.erase(0, nameStr.find_first_not_of(" \t"));
-                    nameStr.erase(nameStr.find_last_not_of(" \t") + 1);
-                    if(!nameStr.empty()) ref.name = nameStr;
+                    ref.name = text.substr(colon + 1, at - colon - 1);
+                    if (ref.name.has_value()) {
+                        auto& s = *ref.name;
+                        s.erase(0, s.find_first_not_of(" \t"));
+                        s.erase(s.find_last_not_of(" \t") + 1);
+                    }
                 }
             }
         }
-        
+
+        if (ref.globalID.has_value()) {
+            int id = ref.globalID.value();
+
+            auto it = state.globalIDMap.find(id);
+            if(it != state.globalIDMap.end()){
+                Node* to = it->second;
+            
+                std::unordered_set<Node*> visited;
+                if(hasPath(to, from, state.refGraph, visited)){
+                    throw pegtl::parse_error(
+                        "Circular reference detected (global)",
+                        in
+                    );
+                }
+            
+                state.refGraph[from].insert(to);
+            }
+        }
+
         auto valNode = std::make_shared<ValueNode>();
         valNode->value = ref;
-        
+
         if(state.inList){
             state.currentList.push_back(valNode);
         } else {
-            state.nodeStack.back()->properties["_last_value"] = std::vector<std::shared_ptr<ValueNode>>{valNode};
+            state.nodeStack.back()->properties["_last_value"] =
+                std::vector<std::shared_ptr<ValueNode>>{valNode};
         }
     }
 };
@@ -221,19 +285,29 @@ template<> struct Action<grammar::node_header> {
         std::string text = in.string();
         text.erase(0, text.find_first_not_of(" \t\r\n"));
         text.erase(text.find_last_not_of(" \t\r\n") + 1);
-        
+
         std::istringstream ss(text);
         std::string tmp, type, name;
         ss >> tmp >> type >> name;
-        
+
         NodePtr node = std::make_shared<Node>();
         node->type = type;
         node->name = name;
-        
+
         std::string token;
         while(ss >> token){
-            if(token[0]=='@') node->globalID = std::stoi(token.substr(1));
-            else if(token[0]=='#') node->localID = std::stoi(token.substr(1));
+            if(token[0]=='@'){
+                node->globalID = std::stoi(token.substr(1));
+                if (node->globalID) {
+                    state.globalIDMap[*node->globalID] = node.get();
+                }
+            }
+            else if(token[0]=='#'){
+                node->localID = std::stoi(token.substr(1));
+                if (node->localID) {
+                    state.localIDMap[*node->localID] = node.get();
+                }
+            }
         }
 
         if(state.nodeStack.empty()){
